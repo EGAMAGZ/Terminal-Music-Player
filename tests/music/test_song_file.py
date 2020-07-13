3947eee95030d50f68e9f93f1d8e469fc3695e93
EGAMAGZ <gazapito1@hotmail.com>
2020-07-12 19:24:47 -0500

    Added test for method is_valid_extension

--- tests/music/test_song_file.py
+++ tests/music/test_song_file.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 
-from pymusicterm.music import SongFile
+from pymusicterm.music import SongFile,is_valid_extension
 
 def join_path(path,file_name) -> str:
     return os.path.join(path,file_name)
@@ -12,9 +12,16 @@ class TestSongFile(unittest.TestCase):
         self.win_path='C:\\Users\\MyName\Music'
         self.linux_path='/home/myuser/Music/'
         self.file_name='song.mp3'
+        self.file_name2='song.wav'
+        self.file_name3='song.mp4'
 
         self.win_song_file=SongFile(self.win_path,self.file_name)
         self.linux_song_file=SongFile(self.linux_path,self.file_name)
+
+    def test_valid_extension(self):
+        self.assertEqual(is_valid_extension(self.file_name),True)
+        self.assertEqual(is_valid_extension(self.file_name2),True)
+        self.assertEqual(is_valid_extension(self.file_name3),False)
 
     def test_get_name(self):
         self.assertEqual(self.win_song_file.get_name(),'song')
