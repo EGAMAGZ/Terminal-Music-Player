8d6f40aafdc3d91a43895fe13ab9b5f85a6fa9aa
EGAMAGZ <gazapito1@hotmail.com>
2020-07-17 19:24:33 -0500

    Documented and commented file.py

--- pymusicterm/util/file.py
+++ pymusicterm/util/file.py
@@ -5,24 +5,52 @@ from pymusicterm.music import SongFile,is_valid_extension
 from pymusicterm.util.system import on_wsl,get_platform_name,get_user_name
 
 class File:
+    """ Class resposible to get the list of SongFile dataclasses for the LocalPlayerWindow
+
+    This class contains functions that are use to search for song files with valid extensions
+    and with them generate lists of SongFile dataclasses that will be used in the window of the
+    local music player, and to change the path to search those files
+    """
 
     _file_path:str
     _platform_name:str
     _user_name:str
 
     def __init__(self):
-        self._platform_name=get_platform_name()
-        self._user_name=get_user_name()
-        self._file_path=self.__get_default_path()
+        self._platform_name=get_platform_name() # Gets platform name
+        self._user_name=get_user_name() # Gets username
+        self._file_path=self.__get_default_path() # By default will get the default path of each platform
 
     def set_file_path(self,file_path:str):
+        """ Sets file path to load songs
+
+        Parameters
+        ----------
+        file_path : str
+            File path where song files will be searched
+        """
         self._file_path=file_path
 
     def get_file_path(self) -> str:
+        """ Get the actual file path
+
+        Returns
+        -------
+        file_path : str
+            Actual file path
+        """
         return self._file_path
 
     def __get_default_path(self) -> str:
+        """ Gets the default music path depending of the platform the program is runned
+
+        Returns
+        -------
+        file_path : str
+            Default music path
+        """
         if not on_wsl():
+            # If the user is not in wsl
             if self._platform_name == "linux":
                 file_path="/home/{}/Music/".format(self._user_name)
             elif self._platform_name == "windows":
@@ -33,20 +61,27 @@ class File:
         return file_path
 
     def _get_songs_file(self) -> List[str]:
-        songs_list=[]
-        files_list=os.listdir(self._file_path)
+        songs_list=[] #List of song files
+        files_list=os.listdir(self._file_path) # List of all files found in the file path
         for file_name in files_list:
-            #TODO: CREATE METHOD TO VALIDATE MUSIC EXTENSION
-            if is_valid_extension(file_name):
+            if is_valid_extension(file_name): # Check if is a valid extension
                 songs_list.append(file_name)
         
         return songs_list
 
     def get_music_list(self) -> List[SongFile]:
-        songs_file_list=[]
+        """ Function that returns a list of SongFile dataclasses
+
+        Returns
+        -------
+        song_files_list : List[SongFile]
+            List of dataclasses with information of the song file
+        """
+        songs_file_list:List[SongFile] = []
         songs_list=self._get_songs_file()
 
         for song in songs_list:
+            # Will add SongFile dataclasses to song_files_list
             songs_file_list.append(SongFile(self._file_path,song))
-        
+
         return songs_file_list
