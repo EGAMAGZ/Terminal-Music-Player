887f68c2c0405771d039833a7cf18006e59507b0
EGAMAGZ <gazapito1@hotmail.com>
2020-07-25 15:46:35 -0500

    Deleted decorator dataclass to SongFile class

--- pymusicterm/music/__init__.py
+++ pymusicterm/music/__init__.py
@@ -1,5 +1,4 @@
 import os
-from dataclasses import dataclass
 
 MUSIC_EXTENSIONS=(".mp3",".wav") # Valid music extensions
 
@@ -8,9 +7,8 @@ def is_valid_extension(file_name:str):
         return True
     return False
 
-@dataclass
 class SongFile:
-    """ Dataclass that stores information of a song file
+    """ Class that stores information of a song file
 
     This class with store the path where is found the song and the name
     of the file. And with the functions will return some requested information
@@ -19,6 +17,10 @@ class SongFile:
     """
     path:str
     file_name:str
+
+    def __init__(self,path:str,file_name:str):
+        self.path=path
+        self.file_name=file_name
 
     def get_name(self) -> str:
         """ Gets the name of the file
