2fe5ac8ca1b95cd03d5b6e8f1e68d8ebb0e0a3b0
EGAMAGZ <gazapito1@hotmail.com>
2020-07-27 19:21:27 -0500

    Modified the tests

--- tests/util/test_file.py
+++ tests/util/test_file.py
@@ -9,34 +9,24 @@ class TestFile(unittest.TestCase):
         self.file_class=File()
         self.username=get_user_name()
         self.win_path='C:\\Users\\{}\Music'.format(self.username)
-        self.wsl_path='/mnt/c/Users/{}/Music'.format(self.username)
         self.linux_path='/home/{}/Music'.format(self.username)
 
     def test_get_default_path(self):
-        self.assertIn(self.file_class.get_file_path(),[self.win_path,
-        self.wsl_path,self.linux_path])
+        self.assertIn(self.file_class.get_file_path(),[self.win_path,self.linux_path])
 
     def test_set_file_path(self):
         # Set default file path for windows
         self.file_class.set_file_path(self.win_path)
         self.assertEqual(self.file_class.get_file_path(),self.win_path)
-        self.assertNotIn(self.file_class.get_file_path(),[self.wsl_path,self.linux_path])
-
-        # Set default file path for wsl
-        self.file_class.set_file_path(self.wsl_path)
-        self.assertEqual(self.file_class.get_file_path(),self.wsl_path)
-        self.assertNotIn(self.file_class.get_file_path(),[self.win_path,self.linux_path])
 
         # Set default file path for linux
         self.file_class.set_file_path(self.linux_path)
         self.assertEqual(self.file_class.get_file_path(),self.linux_path)
-        self.assertNotIn(self.file_class.get_file_path(),[self.win_path,self.wsl_path])
 
     def tearDown(self):
         del(self.file_class)
         del(self.username)
         del(self.win_path)
-        del(self.wsl_path)
         del(self.linux_path)
 
 if __name__ == "__main__":
